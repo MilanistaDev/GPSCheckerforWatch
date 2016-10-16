d6acb015fb7bf5dd02e037e3bf09705beacc2b43
Takuya Aso <milanista224@gmail.com>
2016-10-17 02:22:30 +0900

    [GPSCfW] Implement location info for iOS.

--- GPSCheckerforWatch/ViewController.swift
+++ GPSCheckerforWatch/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -22,11 +23,45 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Access Location Service
+        LocationManager.Singleton.sharedInstance.startUpdatingLocation()
+
+        // Set NSNotificationCenter
+        let center = NotificationCenter.default
+        center.addObserver(self,
+                           selector:#selector(displayData(notification:)),
+                           name:NSNotification.Name(rawValue: LMLocationUpdateNotification),
+                           object:nil)
+    }
+
+    deinit {
+        let center = NotificationCenter.default
+        center.removeObserver(self)
+    }
+
+    // MARK:- Private Method
+    /**
+     位置情報を取得
+     get Location data
+     - parameter notification:通知
+     */
+    func displayData(notification:Notification) {
+        let infoDic: Dictionary = notification.userInfo as Dictionary!
+        let location: CLLocation? = infoDic[LMLocationInfoKey] as? CLLocation
+        let coordinate = location!.coordinate
+
+        self.latLabel.text = (coordinate.latitude).description
+        self.lonLabel.text = (coordinate.longitude).description
+    }
+
+    // MARK:- Memory warning
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
